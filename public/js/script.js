233038c36d0e6320506201b7b49c47a97d852cab
LKQMLR <rossignolyannis@gmail.com>
2024-03-08 00:07:00 +0100

    progression sur le header, navbar : toggle on/off

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerButton = document.querySelector('.burger_button');
+
+    burgerButton.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    })
+})
