bc23c2fe73eb56d57b1f710c49419f8b80db10f6
LKQMLR <rossignolyannis@gmail.com>
2024-03-22 22:10:29 +0100

    mise en forme page admin

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burgerButton = document.querySelector('.burger_button');
+    const adminButton = document.querySelector('.admin_button');
+    const adminPanel = document.querySelector('.admin_panel');
 
     burgerButton.addEventListener('click', () => {
         nav.classList.toggle('active');
+    })
+
+    adminButton.addEventListener('click', () => {
+        adminPanel.classList.toggle('active')
     })
 })
