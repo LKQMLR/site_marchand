5c5059e1a25e9b02b99a8a872f6968703cb8b167
LKQMLR <rossignolyannis@gmail.com>
2024-03-25 23:09:36 +0100

    crud admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,22 +2,11 @@ import query from '../database.js';
 
 export const listAdmin = (req, res) => {
 
-    const id = req.params.id;
-
-    query('SELECT * FROM utilisateurs WHERE user_id = ?',
-    [id],
+    query('SELECT * FROM utilisateurs',
     (error, result) => {
         if(error) {
             console.error(`Erreur lors de l'exécution de la requête : ${error}`);
             return res.status(500).json({error : 'Erreur Serveur'});
-        }
-
-        const admin = {
-            id,
-            name: result[0].prenom,
-            lastname: result[0].nom,
-            email: result[0].email,
-            role: result[0].role 
         }
 
         res.json(result)
--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burgerButton = document.querySelector('.burger_button');
-    const adminButton = document.querySelector('.admin_button');
-    const adminPanel = document.querySelector('.admin_panel');
 
     burgerButton.addEventListener('click', () => {
         nav.classList.toggle('active');
     })
 
-    adminButton.addEventListener('click', () => {
-        adminPanel.classList.toggle('active')
-    })
 })
