f62f8a9ad087dd61e934b0ee8e82c09da18162f2
LKQMLR <rossignolyannis@gmail.com>
2024-03-26 21:57:07 +0100

    crud admin

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    
+    handleNavbar();
+    initializeAdminPanel();
+
+})
+
+function handleNavbar() {
     const burgerButton = document.querySelector('.burger_button');
 
     burgerButton.addEventListener('click', () => {
         nav.classList.toggle('active');
     })
 
-})
+}
+
+
+function initializeAdminPanel() {
+    const adminButton = document.querySelector('.admin_button');
+    const adminPanel = document.querySelector('.admin_panel');
+    
+    function handleAdminButton() {
+        adminPanel.classList.toggle('active');
+    }
+    
+    adminButton.addEventListener('click', handleAdminButton);
+}
